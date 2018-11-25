1ff24a6bcd058e1de53974aeb7f7bb916cb6c08f
scott.liao <shazi7804@gmail.com>
2018-11-26 03:52:43 +0800

    finish

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,20 +10,16 @@ sudo apt-get install jq -y
 
 # install ecs-deploy
 curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
-sudo tee -a /usr/bin/ecs-deploy
-sudo chmod +x /usr/bin/ecs-deploy
+  sudo tee -a /usr/bin/ecs-deploy \
+  sudo chmod +x /usr/bin/ecs-deploy
 
 # login AWS ECR
 eval $(aws ecr get-login --no-include-email --region ${AWS_DEFAULT_REGION})
 
 # build the docker image and push to an image repository
 docker --version
-docker build -t demo/ecs-auto-deploy .
 docker tag demo/ecs-auto-deploy:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECR_REPO}:${ECR_IMAGE_TAG}
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECR_REPO}:${ECR_IMAGE_TAG}
 
 # update an AWS ECS service with the new image
-
-echo "ecs-deploy -c $ECS_CLUSTER_NAME -n $ECS_SERIVCE_NAME -i ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECR_REPO}:${ECR_IMAGE_TAG}"
-
 ecs-deploy -c $ECS_CLUSTER_NAME -n $ECS_SERIVCE_NAME -i ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECR_REPO}:${ECR_IMAGE_TAG}
